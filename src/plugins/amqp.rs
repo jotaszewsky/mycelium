43d6be976a23208375d4879756279aef4405f815
Jotaszewsky <khrusza@gmail.com>
2020-12-23 17:27:23 +0100

    Add tests for queue arguments in amqp plugin

--- src/plugins/amqp.rs
+++ src/plugins/amqp.rs
@@ -111,3 +111,65 @@ fn build_arguments(queue_arguments: &str) -> FieldTable {
     }
     return args;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn build_arguments_not_json_error() {
+        build_field_table(Some(String::from("not_json?")));
+    }
+
+    #[test]
+    fn build_arguments_string() {
+        let fields: FieldTable = build_field_table(
+            Some(
+                String::from("{\"test1\": {\"value\": \"test value 1\", \"typedef\": \"string\"}, \"test2\": {\"value\": \"test value 2\", \"typedef\": \"string\"}}")
+            )
+        );
+        let (first_key, first_value) = fields.iter().next().unwrap();
+        assert_eq!((first_key, first_value), (&String::from("test1"), &AmqpValue::LongString(String::from("test value 1"))));
+        let (second_key, second_value) = fields.iter().last().unwrap();
+        assert_eq!((second_key, second_value), (&String::from("test2"), &AmqpValue::LongString(String::from("test value 2"))));
+    }
+
+    #[test]
+    fn build_arguments_int() {
+        let fields: FieldTable = build_field_table(
+            Some(
+                String::from("{\"test1\": {\"value\": \"1\", \"typedef\": \"int\"}, \"test2\": {\"value\": \"23423\", \"typedef\": \"int\"}}")
+            )
+        );
+        let (first_key, first_value) = fields.iter().next().unwrap();
+        assert_eq!((first_key, first_value), (&String::from("test1"), &AmqpValue::ShortInt(1)));
+        let (second_key, second_value) = fields.iter().last().unwrap();
+        assert_eq!((second_key, second_value), (&String::from("test2"), &AmqpValue::ShortInt(23423)));
+    }
+
+    #[test]
+    fn build_arguments_mixed_int_string() {
+        let fields: FieldTable = build_field_table(
+            Some(
+                String::from("{\"test1\": {\"value\": \"test value 1\", \"typedef\": \"string\"}, \"test2\": {\"value\": \"23423\", \"typedef\": \"int\"}}")
+            )
+        );
+        let (first_key, first_value) = fields.iter().next().unwrap();
+        assert_eq!((first_key, first_value), (&String::from("test1"), &AmqpValue::LongString(String::from("test value 1"))));
+        let (second_key, second_value) = fields.iter().last().unwrap();
+        assert_eq!((second_key, second_value), (&String::from("test2"), &AmqpValue::ShortInt(23423)));
+    }
+
+    #[test]
+    #[should_panic]
+    fn build_arguments_unknow_type_error() {
+        build_field_table(Some(String::from("{\"test1\": {\"value\": \"0.2\", \"typedef\": \"float\"}}")));
+    }
+
+    #[test]
+    fn build_empty_field_table_if_no_json() {
+        let fields: FieldTable = build_field_table(None);
+        assert_eq!(fields.len(), 0);
+    }
+}
