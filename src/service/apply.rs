a39f8d16f2916827d95955f17eba2ac12baf5bb8
Jotaszewsky <khrusza@gmail.com>
2021-01-19 18:17:35 +0100

    Add tests to apply service

--- src/service/apply.rs
+++ src/service/apply.rs
@@ -19,13 +19,47 @@ struct File {
 }
 
 pub fn execute(path: PathBuf) -> Result<(),()> {
-    if !path.is_file() {
-        panic!("Wrong input path");
+    if !path.is_file() || path.extension().unwrap() != "yaml" {
+        panic!("Wrong input path or extension!");
     }
     let mut temp = State::new(None);
-    let file: File = serde_yaml::from_str(&read_to_string(path).unwrap()).unwrap();
+    let file: File = serde_yaml::from_str(&read_to_string(path).unwrap()).expect(
+        "Wrong yamm format!"
+    );
 
     temp.store(String::from(READ_TEMP_KEY), bincode::serialize(&file.input).unwrap())?;
     temp.store(String::from(WRITE_TEMP_KEY), bincode::serialize(&file.output).unwrap())?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn test_not_existing_file() {
+        let path = PathBuf::from("/tmp/test_not_existing_file.yml");
+        execute(path);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_not_yaml_extension() {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("tests/assets/apply/default.json");
+        execute(path);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_not_yaml_format() {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("tests/assets/apply/wrong.yaml");
+        execute(path);
+    }
+
+    /*
+    * For the rest of tests we need test doubles.
+    */
 }
